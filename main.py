3bf8aa48966d043dc302a15b9a5543b263242d89
AleUli <alek-ulitin@yandex.ru>
2021-12-15 19:37:48 +0300

    add. author

--- main.py
+++ main.py
@@ -11,20 +11,35 @@ def generator_book(pk=1):
         pk += 1
 
 
-def title():
+def generator_title():
     with open("books.txt", "rt", encoding="utf-8") as f:
         return random.choice(f.readlines())
 
 
-def year():
+def generator_year():
     return randint(2000, 2050)
 
 
-def pages():
+def generator_pages():
     return randint(1, 1000)
 
 
 def generator_fake():
     fake = Faker()
     return fake.isbn13()
+
+
+def generator_raiting():
+    return round(random.uniform(0.0, 5.0), 1)
+
+
+def generator_price():
+    return round(random.uniform(0.0, 10000.0), 1)
+
+
+def generator_author():
+    fake = Faker()
+    name_author = fake.name()
+    return name_author
+
 
