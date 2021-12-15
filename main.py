e298fb16f57b49bc7f3f1209a6c1e5e864686f6e
AleUli <alek-ulitin@yandex.ru>
2021-12-15 19:28:38 +0300

    polovina

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+from random import randint
+import random
+import json
+from faker import Faker
 from conf import model
 
-def pk(pk = 1):
+
+def generator_book(pk=1):
     while True:
         yield pk
         pk += 1
 
 
+def title():
+    with open("books.txt", "rt", encoding="utf-8") as f:
+        return random.choice(f.readlines())
 
 
+def year():
+    return randint(2000, 2050)
 
 
+def pages():
+    return randint(1, 1000)
 
 
+def generator_fake():
+    fake = Faker()
+    return fake.isbn13()
 
-if __name__ == '__main__':
